4611ce07d8dc80772f7d30bf85db5d78e0082a0e
Oliver Holmberg <63006762+Oliver1526@users.noreply.github.com>
2025-08-31 17:05:03 +0200

    Added searching for positions code

---
+++ Shortest-path-finder.py
@@ -0,0 +1,72 @@
+
+#Setting up maze
+maze = [
+    ["#", "#", "#", "#", "#", "#", "#", "O", "#", "#"],
+    ["#", " ", "#", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", " ", " ", "#", " ", "#", "#", " ", "#"],
+    ["#", "#", "#", " ", " ", " ", "#", " ", " ", "#"],
+    ["#", " ", " ", " ", "#", " ", "#", " ", "#", "#"],
+    ["#", " ", "#", " ", "#", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", " ", "#", "#", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#", "#"],
+    ["#", "#", "#", "#", "#", "X", "#", "#", "#", "#"]
+]
+current_row = 9
+current_index = 5
+
+
+# Visited positions
+visited_positions = []
+
+
+
+# Search for empty position
+def search_for_empty_position(current_row, current_pos):
+    min_row = current_row - 1
+    max_row = current_row + 1
+    
+    min_pos = current_pos - 1
+    max_pos = current_pos + 1
+
+    
+
+    
+    try:
+        # Same row
+        if maze[current_row][min_pos] != "#":
+            move_to_position(current_row, min_pos)
+            return
+        elif maze[current_row][max_pos] != "#":
+            move_to_position(current_row, max_pos)
+            return 
+        
+        # Lower Row
+        elif maze[min_row][current_pos] != "#":
+            move_to_position(min_row, current_pos)
+            return
+        elif maze[min_row][current_pos] != "#":
+            move_to_position(min_row, current_pos)
+            return
+        
+        # Upper Row
+        elif maze[max_row][current_pos] != "#":
+            move_to_position(max_row, current_pos)
+            return
+        elif maze[max_row][current_pos] != "#":
+            move_to_position(max_row, current_pos)
+            return
+        
+        else:
+            return
+    except:
+        return
+
+
+def move_to_position(row, pos):
+    print("Moved")
+    print(row, pos)
+
+search_for_empty_position(current_row, current_index)
+
+
