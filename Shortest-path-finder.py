7332f01aa32d0c8b9988ebaa2f24fbd3def15efe
Oliver Holmberg <63006762+Oliver1526@users.noreply.github.com>
2025-08-31 17:10:38 +0200

    Deleted unnesesary code

--- Shortest-path-finder.py
+++ Shortest-path-finder.py
@@ -31,29 +31,27 @@ def search_for_empty_position(current_row, current_pos):
 
     
 
-    
+    # Fix O search later
     try:
         # Same row
         if maze[current_row][min_pos] != "#":
+            visited_positions.append([current_row, min_pos])
             move_to_position(current_row, min_pos)
             return
         elif maze[current_row][max_pos] != "#":
+            visited_positions.append([current_row, max_pos])
             move_to_position(current_row, max_pos)
             return 
         
         # Lower Row
         elif maze[min_row][current_pos] != "#":
+            visited_positions.append([min_row, current_pos])
             move_to_position(min_row, current_pos)
             return
-        elif maze[min_row][current_pos] != "#":
-            move_to_position(min_row, current_pos)
-            return
-        
+
         # Upper Row
         elif maze[max_row][current_pos] != "#":
-            move_to_position(max_row, current_pos)
-            return
-        elif maze[max_row][current_pos] != "#":
+            visited_positions.append([max_row, current_pos])
             move_to_position(max_row, current_pos)
             return
         
@@ -66,6 +64,7 @@ def search_for_empty_position(current_row, current_pos):
 def move_to_position(row, pos):
     print("Moved")
     print(row, pos)
+    
 
 search_for_empty_position(current_row, current_index)
 
